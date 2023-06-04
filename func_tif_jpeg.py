a0b890c5468137ec551a49fef61252d94f6c35b2
Dmitry <113550281+B3SSS@users.noreply.github.com>
2023-06-04 15:01:20 +0300

    func_tif_jpeg.py

---
+++ func_tif_jpeg.py
@@ -0,0 +1,45 @@
+from os import path
+import os
+from pathlib import Path
+from PIL import Image
+
+def tif_to_jpeg(folder_input, folder_to):
+    if os.path.exists(folder_input) == False:
+        return f'Path does not exist: {folder_input}'
+
+    if os.path.isdir(folder_input) == False:
+        return f'Path is not a directory: {folder_input}'
+
+    files = Path(folder_input).glob('*.tif')
+
+    len_files = len(list(files))
+    if len_files == 0:
+        return f'There are no .tif files in the path: {folder_input}'
+
+    if os.path.exists(folder_to) == False:
+        os.makedirs(folder_to)
+
+    loaded = 0
+    files = Path(folder_input).glob('*.tif')
+    for file in files:
+        full_name = path.basename(file)
+        name = path.splitext(full_name)[0]
+        full_path_input = os.path.join(folder_input, full_name)
+        full_path_out = os.path.join(folder_to, name + '.jpeg')
+
+        try:      
+            im = Image.open(full_path_input)
+            im.save(full_path_out, 'JPEG')
+            print(f'Loaded: {full_path_out}')
+            loaded += 1
+        except Exception as ex:
+            return f'{ex}: {full_path_input}'
+
+    print()
+    return f'Images saved successfully \nLoaded: {loaded}/{len_files} \nPath: {folder_to}'
+
+
+if __name__ == '__main__':
+    paths = input('From/To: ').strip().split()
+    print()
+    print(tif_to_jpeg(paths[0], paths[1]))
